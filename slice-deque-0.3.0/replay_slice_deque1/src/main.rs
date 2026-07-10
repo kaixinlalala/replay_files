de9ed9cb356bc64f5298212a073dd8e96db6d2bd
kaixinlalala <kaixinlalala@users.noreply.github.com>
2026-07-10 20:53:26 +0800

    slice-deque fuzz replay files update(2)

--- slice-deque-0.3.0/replay_slice_deque1/src/main.rs
+++ slice-deque-0.3.0/replay_slice_deque1/src/main.rs
@@ -1,5 +1,13 @@
 extern crate slice_deque;
 fn main() {
-    let mut _local0 = slice_deque:: SliceDeque::<i32>::with_capacity(0);
-    let _ = _local0.as_slices();
+    let mut d = slice_deque::SliceDeque::<i32>::new();
+    d.push_back(1);
+    d.push_back(2);
+    let (left, right) = d.as_slices();
+    assert_eq!(left, &[1, 2]);
+    assert_eq!(right, &[]);
+
+// 第二个同样情况的示例
+    // let mut _local0 = slice_deque:: SliceDeque::<i32>::with_capacity(0);
+    // let _ = _local0.as_slices();
 }
